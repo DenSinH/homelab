b1ee3ac45bef033560ddadc380f712667c0eb2d9
Dennis Hilhorst <dhilhorst@highvolteng.com>
2026-06-28 15:23:45 +0000

    fixes

--- scripts/config-igpu.nix
+++ scripts/config-igpu.nix
@@ -17,14 +17,13 @@ configBase.mkConfigScript {
   addLines = ''
     print_info "Detecting DRM devices on host..."
 
-    CARDS=(/dev/dri/card*)
+    # Initialize variables to avoid unbound variable errors
+    CARD_PATH=""
+    CARD_NAME=""
 
-    if compgen -G "/dev/dri/card*" > /dev/null; then
-      CARD_PATH="$(ls /dev/dri/card* | head -n1)"
-      CARD_NAME="$(basename "$CARD_PATH")"
-    else
-      CARD_PATH=""
-      CARD_NAME=""
+    if compgen -G "/dev/dri/card*" > /dev/null 2>&1; then
+      CARD_PATH="\$(ls /dev/dri/card* 2>/dev/null | head -n1)"
+      CARD_NAME="\$(basename "\$CARD_PATH")"
     fi
 
     if [ ! -e "/dev/dri/renderD128" ]; then
@@ -35,8 +34,8 @@ configBase.mkConfigScript {
     # --- Add config lines dynamically ---
     add_line "lxc.cgroup2.devices.allow: c 226:1 rwm"
 
-    if [ -n "$CARD_NAME" ]; then
-      add_line "lxc.mount.entry: /dev/dri/$CARD_NAME dev/dri/$CARD_NAME none bind,create=file 0 0"
+    if [ -n "\$CARD_NAME" ]; then
+      add_line "lxc.mount.entry: /dev/dri/\$CARD_NAME dev/dri/\$CARD_NAME none bind,create=file 0 0"
     fi
 
     add_line "lxc.cgroup2.devices.allow: c 226:128 rwm"
